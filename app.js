08cebe8fc611fd918a06e361aca14575ff0a4e73
Sudheer J <sudheerjillellamudi123@gmail.com>
2024-10-26 20:08:07 +0530

    added server files

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const cors = require("cors");
+const app = express()
+const port = 5000
+
+const API_KEY = "key"
+app.use(cors())
+
+app.get('/news/:country?/:category?' , (req , rsp) =>{
+    url = urlBuilder(req.params.country , req.params.category)
+    var request = new Request(url)
+
+    fetch(request)
+        .then(function(response) {
+
+            if(!response.ok){
+                throw new Error(`Response status not 200 status ${response.status}`)
+            }
+
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+            rsp.send(data)
+        })
+        .catch((e)=>{
+            console.log(`Fetch error: ` , e)
+            rsp.status(500).send("Failed to fetch data")
+        })
+})
+
+
+function urlBuilder(country , category) {
+    let url = `https://newsapi.org/v2/top-headlines`
+
+    if(category) {
+        url += `?category=${category}`
+        url += `&apiKey=${API_KEY}`
+        return url
+    }
+
+    if(country) {
+        url += `?country=${country}`
+    }else {
+        url += `?country=us`
+    }
+
+    url += `&apiKey=${API_KEY}`
+
+    return url
+}
+
+app.listen(port , ()=>{console.log(`Listing on ${port} port`)})
